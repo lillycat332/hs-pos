912df10eb9672794a456fdf80d3aad7c96b0cec6
Lilly <54189319+lillycat332@users.noreply.github.com>
2022-06-17 11:57:09 +0100

    Switch to using Beam database

--- app/Main.hs
+++ app/Main.hs
@@ -1,12 +1,20 @@
-{-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE OverloadedStrings
+           , DeriveGeneric
+           , ScopedTypeVariables 
+           , StandaloneDeriving 
+           , TypeSynonymInstances 
+           , MultiParamTypeClasses
+#-}
 module Main where
-
 import Control.Applicative ((<$>))
 import Control.Monad (join)
 import Data.Aeson (ToJSON)
+import Data.Int
 import Data.Maybe (fromMaybe)
 import Data.Monoid (mconcat)
+import Data.Text
+import Database.Beam
+import Database.Beam.Sqlite
 import GHC.Generics
 import Network.HTTP.Types
 import Network.Wai.Middleware.RequestLogger (logStdoutDev)
@@ -14,9 +22,14 @@ import Network.Wai.Middleware.Static (addBase, noDots, staticPolicy, (>->))
 import System.Environment (lookupEnv)
 import Text.Read (readMaybe)
 import Web.Scotty
+import Control.Applicative
+import Data.Semigroup ((<>))
 
 main :: IO ()
 main = do
+  -- conn <- connectSqlite3 "store.db"
+  -- run conn "CREATE TABLE IF NOT EXISTS users(id INTEGER AUTO_INCREMENT, Username CHAR, HashedPassword CHAR, Privilege INTEGER);" []
+  -- disconnect conn
   port <- fromMaybe 3000
     . join
     . fmap readMaybe <$> lookupEnv "PORT"
@@ -24,23 +37,37 @@ main = do
   scotty port $ do
     middleware $ staticPolicy (noDots >-> addBase "public")
     middleware logStdoutDev
-    home >> adm >> prod >> usr
+    home >> adm
 
-data Product = Product
-  { productId :: Int
-  , productName :: String
-  , productPrice :: Double
-  } deriving Generic
+-- data Product = Product
+--   { productId :: Int
+--   , productName :: String
+--   , productPrice :: Double
+--   } deriving Generic
 
-data User = User
-  { userId :: Int
-  , userName :: String
-  , hashedPassword :: String
-  , privilege :: Int
-  } deriving Generic
+data UserT f
+  = User
+  { _userName      :: Columnar f Text
+  , _userPassword  :: Columnar f Text
+  , _userPrivilege :: Columnar f Int32
+  }
+  deriving Generic
 
-instance ToJSON Product
-instance ToJSON User
+type User = UserT Identity
+type UserId = PrimaryKey UserT Identity
+instance Beamable UserT
+instance Table UserT where
+   data PrimaryKey UserT f = UserId (Columnar f Text) deriving (Generic, Beamable)
+   primaryKey = UserId . _userEmail
+
+data userDb f = userDb
+              { _users :: f (TableEntity UserT) }
+                deriving (Generic, Database be)
+
+userDb :: DatabaseSettings be userDb
+userDb = defaultDbSettings
+
+-- Connection handler
 
 home :: ScottyM ()
 home = get "/" $ file "./public/index.html"
@@ -48,9 +75,24 @@ home = get "/" $ file "./public/index.html"
 adm :: ScottyM ()
 adm = get "/" $ file "./public/dash.html"
 
-prod :: ScottyM ()
-prod = get "/prods" $ json $ Product 1 "Strawberries" 1.99
+-- prod :: ScottyM ()
+-- prod = get "/prods" $ json "hi"
 
-usr :: ScottyM ()
-usr = get "/users" $ json $ User 1 "Admin" "NULL" 1
+-- usr :: ScottyM ()
+-- usr = get "/users" $ json "bye"
+
+-- Database handling
+
+addUser :: String -> String -> Integer -> IO [[SqlValue]]
+addUser name pw priv = 
+  do
+    conn <- connectSqlite3 "store.db"
+    state <- prepare conn "INSERT INTO users VALUES (?,?,?)" 
+    execute state [toSql name, toSql pw, toSql priv]
+    result <- fetchAllRows' state
+    putStrLn $ show result
+
+    disconnect conn
+
+    return result
 
--- default.nix
+++ default.nix
@@ -1,35 +1,81 @@
-# { pkgs, lib, stdenv, fetchFromGitHub }:
 let
   pkgs = import <nixpkgs> { };
   stdenv = pkgs.llvmPackages_13.stdenv;
 in
-pkgs.stdenv.mkDerivation {
-  name = "hs-pos";
-  version = "0.1.0";
 
-  # src = pkgs.fetchFromGitHub {
-  #   owner = "lillycat332";
-  #   repo = "hs-pos";
-  #   rev = "master";
-  #   hash = "";
-  # };
-
-  buildInputs = with pkgs; [
-    clang
-    zlib
-    cabal-install
-    ghc
-    sqlite
+{ mkDerivation
+, aeson
+, base
+, beam-core
+, beam-migrate
+, beam-sqlite
+, HDBC
+, HDBC-sqlite3
+, http-types
+, lib
+, scotty
+, sqlite-simple
+, text
+, wai
+, wai-extra
+, wai-middleware-static
+}:
+let
+  pkgs = import <nixpkgs> { };
+  stdenv = pkgs.llvmPackages_13.stdenv;
+in
+mkDerivation {
+  pname = "hs-pos";
+  version = "0.1.0.0";
+  src = /devel/hs-pos;
+  isLibrary = false;
+  isExecutable = true;
+  executableHaskellDepends = [
+    aeson
+    base
+    beam-core
+    beam-migrate
+    beam-sqlite
+    HDBC
+    HDBC-sqlite3
+    http-types
+    scotty
+    sqlite-simple
+    text
+    wai
+    wai-extra
+    wai-middleware-static
   ];
-
-  buildPhase = ''
-    cabal build
-  '';
-
-  meta = {
-    description = "PoS (point of sale) system in haskell";
-    # homepage = "https://github.com/lillycat332/hs-pos";
-    license = "BSD";
-  };
+  license = lib.licenses.bsd3;
 }
+
+# pkgs.stdenv.mkDerivation {
+#   name = "hs-pos";
+#   version = "0.1.0";
+
+#   # src = pkgs.fetchFromGitHub {
+#   #   owner = "lillycat332";
+#   #   repo = "hs-pos";
+#   #   rev = "master";
+#   #   hash = "";
+#   # };
+
+#   buildInputs = with pkgs; [
+#     clang
+#     zlib
+#     cabal-install
+#     ghc
+#     sqlite
+#   ];
+
+#   buildPhase = ''
+#     cabal build
+#   '';
+
+#   meta = {
+#     description = "PoS (point of sale) system in haskell";
+#     # homepage = "https://github.com/lillycat332/hs-pos";
+#     license = "BSD";
+#   };
+# }
 
