b477c6bb77a5ed05b21aa536a67d02cae3a4068c
Lilly <lilly@lillycat.tech>
2022-06-20 09:16:25 +0100

    hs-pos: formatting

--- app/Main.hs
+++ app/Main.hs
@@ -44,51 +44,6 @@ main = do
     middleware logStdoutDev
     home >> adm
 
-data ProductT f 
-  = Product
-  { _productId     :: Columnar f Int32
-  , _productName    :: Columnar f Text
-  , _productPrice   :: Columnar f Double
-  } 
-  deriving (Generic, Beamable)
-
-type Product = ProductT Identity
-type ProductId = PrimaryKey UserT Identity
-
-instance Table ProductT where
-   data PrimaryKey UserT f = ProductId (Columnar f Int32) deriving (Generic, Beamable)
-   primaryKey = ProductId . _productId
-
-productDb :: DatabaseSettings be ProductDb
-productDb = defaultDbSettings
-
-data ProductDb f = ProductDb
-              { _products :: f (TableEntity ProductT) }
-                deriving (Generic, Database be)
-
-data UserT f
-  = User
-  { _userId        :: Columnar f Int32
-  , _userName      :: Columnar f Text
-  , _userPassword  :: Columnar f Text
-  , _userPrivilege :: Columnar f Int32
-  }
-  deriving (Generic, Beamable)
-
-type User = UserT Identity
-type UserId = PrimaryKey UserT Identity
-
-instance Table UserT where
-   data PrimaryKey UserT f = UserId (Columnar f Int32) deriving (Generic, Beamable)
-   primaryKey = UserId . _userId
-
-userDb :: DatabaseSettings be UserDb
-userDb = defaultDbSettings
-
-data UserDb f = UserDb
-              { _users :: f (TableEntity UserT) }
-                deriving (Generic, Database be)
-
 -- Connection handler
 
 home :: ScottyM ()
