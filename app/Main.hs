95cad67140e41e874d97d7e5abdc4db4320fcbc0
Lilly <lilly@lillycat.tech>
2022-06-23 13:21:16 +0100

    everytyhing broke

--- app/Main.hs
+++ app/Main.hs
@@ -11,14 +11,14 @@ module Main where
 import Control.Applicative
 import Control.Applicative ((<$>))
 import Control.Monad (join)
-import Data.Aeson (ToJSON)
+import qualified Data.Aeson as Aeson
 import Data.Int
 import Data.Maybe (fromMaybe)
 import Data.Monoid (mconcat)
 import Data.Semigroup ((<>))
-import Data.Text
-import Database.Beam
+import Data.Text (Text)
 import Database.Beam.Sqlite
+import Database.Beam
 import Database.SQLite.Simple
 import GHC.Generics
 import Network.HTTP.Types
@@ -28,12 +28,13 @@ import System.Environment (lookupEnv)
 import Text.Read (readMaybe)
 import Web.Scotty
 
+
 main :: IO ()
 main = do
-  -- conn <- open "store.db"
-  -- runBeamSqliteDebug putStrLn conn $ runInsert $
-  --   insert (_users userDb) $
-  --   insertValues [ User "Admin" "NULL" 2]
+  conn <- open "store.db"
+  runBeamSqliteDebug putStrLn conn $ runInsert $
+    insert (_users userDb) $
+    insertValues [ User 0 "Admin" "NULL" 2]
 
   port <- fromMaybe 3000
     . join
@@ -51,3 +52,57 @@ home = get "/" $ file "./public/index.html"
 
 adm :: ScottyM ()
 adm = get "/" $ file "./public/dash.html"
+
+-- usrs :: ScottyM ()
+-- usrs = Web.Scotty.text $ show allUsers
+
+data ProductT f 
+  = Product
+  { _productId     :: Columnar f Int32
+  , _productName    :: Columnar f Text
+  , _productPrice   :: Columnar f Double
+  }
+  	deriving (Generic, Beamable)
+
+type Product = ProductT Identity
+type ProductId = PrimaryKey UserT Identity
+
+instance Table ProductT where
+   data PrimaryKey ProductT f = ProductId (Columnar f Int32) deriving (Generic, Beamable)
+   primaryKey = ProductId . _productId
+
+productDb :: DatabaseSettings be ProductDb
+productDb = defaultDbSettings
+
+data ProductDb f = ProductDb
+              { _products :: f (TableEntity ProductT) }
+                deriving (Generic, Database be)
+
+data UserT f
+  = User
+  { _userId        :: Columnar f Int32
+  , _userName      :: Columnar f Text
+  , _userPassword  :: Columnar f Text
+  , _userPrivilege :: Columnar f Int32 }
+  deriving ( Generic, Beamable )
+
+type User = UserT Identity
+type UserId = PrimaryKey UserT Identity
+
+instance Table UserT where
+   data PrimaryKey UserT f = UserId (Columnar f Int32) deriving (Generic, Beamable)
+   primaryKey = UserId . _userId
+
+instance Aeson.ToJSON User where
+  toJSON (User _userId _userName _userPassword _userPrivilege) = 
+    object [ "id" .= _userId
+           , "name" .= _userName
+           , "passwd" .= _userPassword
+           , "privilege" .= _userPrivilege
+           ]
+userDb :: DatabaseSettings be UserDb
+userDb = defaultDbSettings
+
+data UserDb f = UserDb
+              { _users :: f (TableEntity UserT)  }
+                deriving    (Generic, Database be)
--- default.nix
+++ default.nix
@@ -1,20 +1,58 @@
-let
-  pkgs = import <nixpkgs> { };
-  stdenv = pkgs.llvmPackages_13.stdenv;
-in
+# let
+#   pkgs = import <nixpkgs> { };
+#   stdenv = pkgs.llvmPackages_13.stdenv;
+# in
 
-pkgs.stdenv.mkDerivation
-{
-  name = "hs-pos";
-  version = "0.1.0";
+# pkgs.stdenv.mkDerivation
+# {
+#   name = "hs-pos";
+#   version = "0.1.0";
 
-  # src = pkgs.fetchFromGitHub {
-  #   owner = "lillycat332";
-  #   repo = "hs-pos";
-  #   rev = "master";
-  #   hash = "";
-  # };
+#   src =  ./.;
+#   # pkgs.fetchFromGitHub {
+#   #   owner = "lillycat332";
+#   #   repo = "hs-pos";
+#   #   rev = "master";
+#   #   hash = "sha256-r2kPFE4u97G/PlC3wKu0JwZCq++iFM1bc3rNzhlxPIc=";
+#   # };
 
+#   buildInputs = with pkgs; [
+#     clang
+#     zlib
+#     cabal-install
+#     ghc
+#     sqlite
+#   ];
+
+#   isLibrary = false;
+#   isExecutable = true;
+
+#   buildPhase = ''
+#     cabal build
+#   '';
+
+#   meta = {
+#     description = "PoS (point of sale) system in haskell";
+#     homepage = "https://github.com/lillycat332/hs-pos";
+#     license = lib.licenses.bsd3;
+#   };
+# }
+
+{ mkDerivation, aeson, base, beam-core, beam-migrate, beam-sqlite
+, HDBC, HDBC-sqlite3, http-types, lib, optparse-applicative, scotty
+, sqlite-simple, text, wai, wai-extra, wai-middleware-static, pkgs, ...
+}:
+mkDerivation {
+  pname = "hs-pos";
+  version = "0.1.0.0";
+  src = ./.;
+  isLibrary = false;
+  isExecutable = true;
+  executableHaskellDepends = [
+    aeson base beam-core beam-migrate beam-sqlite HDBC HDBC-sqlite3
+    http-types optparse-applicative scotty sqlite-simple text wai
+    wai-extra wai-middleware-static
+  ];
   buildInputs = with pkgs; [
     clang
     zlib
@@ -22,15 +60,5 @@ pkgs.stdenv.mkDerivation
     ghc
     sqlite
   ];
-
-  buildPhase = ''
-    cabal build
-  '';
-
-  meta = {
-    description = "PoS (point of sale) system in haskell";
-    homepage = "https://github.com/lillycat332/hs-pos";
-    license = "BSD";
-  };
+  license = lib.licenses.bsd3;
 }
-
