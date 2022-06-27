4232ece5c930a7b0d348a7844e6c540db16a41eb
Lilly <lilly@lillycat.tech>
2022-06-27 20:19:29 +0100

    Add: command line options

--- app/Main.hs
+++ app/Main.hs
@@ -11,39 +11,67 @@ module Main where
 import Control.Applicative
 import Control.Applicative ((<$>))
 import Control.Monad (join)
+import Data.Aeson ((.=))
 import qualified Data.Aeson as Aeson
 import Data.Int
 import Data.Maybe (fromMaybe)
 import Data.Monoid (mconcat)
 import Data.Semigroup ((<>))
 import Data.Text (Text)
-import Database.Beam.Sqlite
-import Database.Beam
-import Database.SQLite.Simple
+import Database.HDBC
+import Database.HDBC.Sqlite3
 import GHC.Generics
 import Network.HTTP.Types
 import Network.Wai.Middleware.RequestLogger (logStdoutDev)
 import Network.Wai.Middleware.Static (addBase, noDots, staticPolicy, (>->))
+import Options.Applicative hiding (header)
+import qualified Options.Applicative as Opt
 import System.Environment (lookupEnv)
 import Text.Read (readMaybe)
 import Web.Scotty
+          
+db = strOption
+       (  long "db"
+       <> short 'd'
+       <> help "Path to the database store"
+       <> showDefault
+       <> value "store.db"
+       <> metavar "<path to file>"
+       )
 
+port = option auto
+       (  long "port"
+       <> short 'p'
+       <> help "The port number to serve on"
+       <> showDefault
+       <> value 3000
+       <> metavar "<1-65535>"
+       )
+
+data Args = Args
+  { optDb     :: String
+  , optPort   :: Int
+  }
+
+args :: Parser Args
+args = Args <$> db <*> port
 
 main :: IO ()
 main = do
-  conn <- open "store.db"
-  runBeamSqliteDebug putStrLn conn $ runInsert $
-    insert (_users userDb) $
-    insertValues [ User 0 "Admin" "NULL" 2]
-
-  port <- fromMaybe 3000
-    . join
-    . fmap readMaybe <$> lookupEnv "PORT"
+  opts <- execParser opts
+  let port = optPort opts
 
   scotty port $ do
     middleware $ staticPolicy (noDots >-> addBase "public")
     middleware logStdoutDev
     home >> adm
+  
+  where
+    opts = info (args <**> helper)
+      (  fullDesc
+      <> progDesc "Run a server for an EPOS system, with a REST API and Database."
+      <> Opt.header "hs-pos -- A Haskell EPOS backend" )
+
 
 -- Connection handler
 
@@ -53,56 +81,25 @@ home = get "/" $ file "./public/index.html"
 adm :: ScottyM ()
 adm = get "/" $ file "./public/dash.html"
 
--- usrs :: ScottyM ()
--- usrs = Web.Scotty.text $ show allUsers
-
-data ProductT f 
+data Product
   = Product
-  { _productId     :: Columnar f Int32
-  , _productName    :: Columnar f Text
-  , _productPrice   :: Columnar f Double
-  }
-  	deriving (Generic, Beamable)
-
-type Product = ProductT Identity
-type ProductId = PrimaryKey UserT Identity
-
-instance Table ProductT where
-   data PrimaryKey ProductT f = ProductId (Columnar f Int32) deriving (Generic, Beamable)
-   primaryKey = ProductId . _productId
-
-productDb :: DatabaseSettings be ProductDb
-productDb = defaultDbSettings
-
-data ProductDb f = ProductDb
-              { _products :: f (TableEntity ProductT) }
-                deriving (Generic, Database be)
+  { _productId      :: Integer
+  , _productName    :: Text
+  , _productPrice   :: Double
+  } deriving         ( Generic )
 
 data UserT f
   = User
-  { _userId        :: Columnar f Int32
-  , _userName      :: Columnar f Text
-  , _userPassword  :: Columnar f Text
-  , _userPrivilege :: Columnar f Int32 }
-  deriving ( Generic, Beamable )
+  { userId        :: Integer
+  , userName      :: Text
+  , userPassword  :: Text
+  , userPrivilege :: Integer 
+  } deriving       ( Generic )
 
-type User = UserT Identity
-type UserId = PrimaryKey UserT Identity
-
-instance Table UserT where
-   data PrimaryKey UserT f = UserId (Columnar f Int32) deriving (Generic, Beamable)
-   primaryKey = UserId . _userId
-
-instance Aeson.ToJSON User where
-  toJSON (User _userId _userName _userPassword _userPrivilege) = 
-    object [ "id" .= _userId
-           , "name" .= _userName
-           , "passwd" .= _userPassword
-           , "privilege" .= _userPrivilege
-           ]
-userDb :: DatabaseSettings be UserDb
-userDb = defaultDbSettings
-
-data UserDb f = UserDb
-              { _users :: f (TableEntity UserT)  }
-                deriving    (Generic, Database be)
+-- instance Aeson.ToJSON User where
+--   toJSON (User _userId _userName _userPassword _userPrivilege) = 
+--     Aeson.object [ "id" .= _userId
+--                  , "name" .= _userName
+--                  , "passwd" .= _userPassword
+--                  , "privilege" .= _userPrivilege
+--                  ]
