5e528df57dddd58d48461f2307765f46ff0abfd7
Lilly <lilly@lillycat.tech>
2022-07-05 14:51:52 +0100

    Update license name

--- app/Main.hs
+++ app/Main.hs
@@ -1,3 +1,40 @@
+{- 
+hs-pos
+Main.hs
+Created by Lilly Cham 
+Copyright (c) 2022, Lilly Cham
+
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+    * Redistributions of source code must retain the above copyright
+      notice, this list of conditions and the following disclaimer.
+
+    * Redistributions in binary form must reproduce the above
+      copyright notice, this list of conditions and the following
+      disclaimer in the documentation and/or other materials provided
+      with the distribution.
+
+    * Neither the name of Lilly Cham nor the names of other
+      contributors may be used to endorse or promote products derived
+      from this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+"AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+-}
+
 {-# LANGUAGE DeriveAnyClass
            , DeriveGeneric
            , MultiParamTypeClasses
@@ -9,6 +46,8 @@
            , TypeApplications
            , OverloadedLabels
 #-}
+
+
 module Main where      
 import Control.Applicative
 import Control.Applicative ((<$>))
@@ -99,8 +138,10 @@ main = do
 home :: ScottyM ()
 home = get "/" $ file "./public/index.html"
 
+
 adm :: ScottyM ()
 adm = get "/dashboard" $ file "./public/dash.html"
+
 
 restHandle :: ScottyM ()
 restHandle = do 
@@ -121,6 +162,7 @@ restHandle = do
     html "delete"
   put "/prods/:u" $ do
     html "put"
+
 
 -- saleHandle :: ScottyM () 
 -- saleHandle = do
@@ -194,7 +236,6 @@ data User_till_xref = User_till_xref
 -- d, a date in the format "YYYY-MM",
 -- id, a product id, 
 -- and returns the total sales for that month.
-
 monthlySales :: String -> String -> Int -> IO [[SqlValue]]
 monthlySales c d id = do
   conn <- connectSqlite3 c
